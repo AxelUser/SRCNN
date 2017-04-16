91262011e7c444393d263defa5940356b24a221a
Алексей Мальцев <maltsevaa@proitr.ru>
2017-04-16 15:52:36 +0500

    Init solution

---
+++ Source/ImageSuperResolution/ImageSuperResolution.SRCNN.WinService/SRCNNService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageSuperResolution.SRCNN.WinService
+{
+    public partial class SRCNNService : ServiceBase
+    {
+        public SRCNNService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ Source/ImageSuperResolution/src/ImageSuperResolution.SRCNN.Handler/Upscalling/ImageChannels.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageSuperResolution.SRCNN.Handler.Upscalling
+{
+    public class ImageChannels
+    {
+    }
+}
